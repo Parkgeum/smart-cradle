0f5f9552a910c55751f9272464ca221fd277a957
Parkgeum <37042238+Parkgeum@users.noreply.github.com>
2019-05-12 21:55:31 +0900

    라즈베리파이-서버 연동 코드

--- raspi_connect_server.py
+++ raspi_connect_server.py
@@ -4,13 +4,13 @@ import sys
 import requests
 import json
 
-
-testurl = 'http://223.194.128.82:80/test/'
+#위험상황 발생시 server에 메시지 전송
+testurl = 'http://223.194.128.82:80/test/'  #서버 주소 수정
 data = {'msg': 'Warnnnnnnnnn!!'}
 headers = {'Content-type':'application/json', 'Accept':'text/plain'}
 r = requests.post(testurl, data=json.dumps(data), headers=headers)
 
-
-ctlurl = 'http://223.194.128.82:80/test/ctl/'
+#서버에서 받은 명령어 실행
+ctlurl = 'http://223.194.128.82:80/test/ctl/' #서버 주소 
 data = requests.get(ctlurl)
 os.system(data.text)
