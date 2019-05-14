97487b7b7b516e409c1e25cd78809ac4c4e7df89
jinkkongee <43057166+jinkkongee@users.noreply.github.com>
2019-05-14 22:09:04 +0900

    Update mysleepforvided.py

--- mysleepforvided.py
+++ mysleepforvided.py
@@ -9,28 +9,32 @@ import imutils
 import time
 import dlib
 import cv2
+import datetime
+import threading
 
 
 sleep = 0
 sleep_start=0
 sleep_end=0
 sleep_time=0
+now = datetime.datetime.now().strftime('%d_%H-%M-%S')
+
 
 def eye_aspect_ratio(eye):
-    # compute the euclidean distances between the two sets of
-    # vertical eye landmarks (x, y)-coordinates
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
  
-    # compute the euclidean distance between the horizontal
-    # eye landmark (x, y)-coordinates
-    C = dist.euclidean(eye[0], eye[3])
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
  
-    # compute the eye aspect ratio
-    ear = (A + B) / (2.0 * C)
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
  
-    # return the eye aspect ratio
-    return ear
+	# return the eye aspect ratio
+	return ear
 
 def caculate_sleep(start,end):
 
@@ -49,7 +53,7 @@ def caculate_sleep(start,end):
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=True)
 ap.add_argument("-v", "--picamera", type=int, default=-1,
-    help="path to input video file")
+	help="path to input video file")
 args = vars(ap.parse_args())
 
 # define two constants, one for the eye aspect ratio to indicate
@@ -85,61 +89,70 @@ time.sleep(1.0)
 
 # loop over frames from the video stream
 while True:
-    # if this is a file video stream, then we need to check if
-    # there any more frames left in the buffer to process
-    if fileStream and not vs.more():
-        break
+	# if this is a file video stream, then we need to check if
+	# there any more frames left in the buffer to process
+	if fileStream and not vs.more():
+		break
  
-    # grab the frame from the threaded video file stream, resize
-    # it, and convert it to grayscale
-    # channels)
-    frame = vs.read()
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# grab the frame from the threaded video file stream, resize
+	# it, and convert it to grayscale
+	# channels)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  
-    # detect faces in the grayscale frame
-    rects = detector(gray, 0)
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
 
-
-    # loop over the face detections
-    for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
- 
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
- 
-        # average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
-
-        # compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-
-        # check to see if the eye aspect ratio is below the blink
-        # threshold, and if so, increment the blink frame counter
-
+	
         
-        if ear < EYE_AR_THRESH:  
+        if len(rects)==0:
+                time.sleep(5)
+                print('nono')
+                cv2.imwrite('./Image/'+str(now)+'.png',frame)
+
+
+                
+
+	# loop over the face detections
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+ 
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+ 
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+		# check to see if the eye aspect ratio is below the blink
+		# threshold, and if so, increment the blink frame counter
+
+		
+		if ear < EYE_AR_THRESH:  
                     sleep=1
                     if sleep_start==0:
                             sleep_start=time.time()
                     
-        if sleep==1:
+		if sleep==1:
                     if ear>EYE_AR_THRESH:
                         sleep=0
                     
-                else: #sleep=0
+                else:
                     if sleep_start!=0 and sleep_end==0:
                         sleep_end=time.time()
                         sleep_time = caculate_sleep(sleep_start,sleep_end)
@@ -165,15 +178,16 @@ while True:
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) 
-    # show the frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	
  
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
  
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
-
