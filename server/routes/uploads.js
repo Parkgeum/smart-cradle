903f2c636fdb198a105fe8b4173af59514776b73
jinkkongee <43057166+jinkkongee@users.noreply.github.com>
2019-05-21 18:41:46 +0900

    Add files via upload

---
+++ server/routes/uploads.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest:'uploads/'})
+var mongoose = require('mongoose');
+var con = require('./../con1');
+var mongo=con.mongo;
+var http = require('http').Server(express);
+var fs = require('fs');
+var exec = require('child_process').exec;
+var cmd='';
+
+
+router.post('/uploads',upload.array('file'),function(req,res,next){
+
+  console.log('hi');
+});
+
+
+module.exports = router;
