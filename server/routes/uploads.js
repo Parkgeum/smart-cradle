4684fcb7552a736f25e43f89582f7df51b81b62b
jinkkongee <43057166+jinkkongee@users.noreply.github.com>
2019-05-25 15:54:29 +0900

    Update uploads.js

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -10,11 +10,24 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var cmd='';
 
-
+//uploads라는 파일 미리 만들어야한다
 router.post('/uploads',upload.array('file'),function(req,res,next){
 
   //console.log('hi');
 });
+//저장 이름및 경로 설정
+let storage = multer.diskStorage({
+  destination: function(req, file ,callback){
+    callback(null, "upload/")
+  },
+  filename: function(req, file, callback){
+    callback(null, file.originalname + " - " + Date.now())
+  }
+})
+
+let upload = multer({
+  storage: storage
+})
 
 
 module.exports = router;
