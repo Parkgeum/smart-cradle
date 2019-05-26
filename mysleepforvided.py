bffe568f53465040a7592d4f5367770820b189f5
jinkkongee <43057166+jinkkongee@users.noreply.github.com>
2019-05-26 17:44:25 +0900

    Update mysleepforvided.py

--- mysleepforvided.py
+++ mysleepforvided.py
@@ -11,41 +11,52 @@ import dlib
 import cv2
 import datetime
 import threading
+import os
+import sys
 import requests
 import json
-
 
 
 sleep = 0
 sleep_start=0
 sleep_end=0
 sleep_time=0
-#now = datetime.datetime.now().strftime('%d_%H-%M-%S')
-#url = 'http://223.194.134.64:80/uploads/uploads'
-url ='http://223.194.132.29:80'
-headers = {'Authorization':'Bearer {}',}
+ear=0
+
+SERVER ='http://223.194.132.29:80'
+CAMURL ='http://223.194.132.29:8090/?action=stream'
+ControlURL = SERVER + '/ctl'
+IMGURL = SERVER + '/uploadimage'
+
+
 command_headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+IMGheaders = {'Authorization':'Bearer {}',}
+
+
 def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
+    
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    
+    
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+    
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
  
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
-	C = dist.euclidean(eye[0], eye[3])
- 
-	# compute the eye aspect ratio
-	ear = (A + B) / (2.0 * C)
- 
-	# return the eye aspect ratio
-	return ear
+    # return the eye aspect ratio
+    return ear
+
 
 def caculate_sleep(start,end):
 
     start_time = start
     end_time = end
-
+    
     sleep_time = end_time-start_time
     sleep_time = round(sleep_time)
 
@@ -58,7 +69,7 @@ def caculate_sleep(start,end):
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=True)
 ap.add_argument("-v", "--picamera", type=int, default=-1,
-	help="path to input video file")
+    help="path to input video file")
 args = vars(ap.parse_args())
 
 # define two constants, one for the eye aspect ratio to indicate
@@ -77,6 +88,7 @@ print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
+
 # grab the indexes of the facial landmarks for the left and
 # right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
@@ -86,126 +98,130 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 
 # start the video stream thread
 print("[INFO] starting video stream thread...")
-# vs = FileVideoStream(args["video"]).start()
-# fileStream = True
-# vs = VideoStream(src=0).start()
 vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 fileStream = False
 time.sleep(1.0)
-
-#CRYINGON
-url_cry = url+'/cry'
+url_cry = SERVER+'/cry'
 data = {'msg': 'CRYON'}
 r = requests.post(url_cry,data=json.dumps(data), headers=command_headers)
 time.sleep(1)
 
 # loop over frames from the video stream
 while True:
-	# if this is a file video stream, then we need to check if
-	# there any more frames left in the buffer to process
-	if fileStream and not vs.more():
-		break
+    # if this is a file video stream, then we need to check if
+    # there any more frames left in the buffer to process
+    
+    if fileStream and not vs.more():
+        break
  
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale
-	# channels)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
-	# detect faces in the grayscale frame
-	rects = detector(gray, 0)
+    
+    # grab the frame from the threaded video file stream, resize
+    # it, and convert it to grayscale
+    # channels)
+    cam = cv2.VideoCapture(CAMURL)
+    ret_val, frame = cam.read()  
 
-	
-        
-        if len(rects)==0:
-                time.sleep(5)
-                print('nono')
-                cv2.imwrite('./Image/'+str(count)+'.png',frame)
-                url_img = url+'/uploadimage'
-                files = {'file':open('/home/pi/Image/'+str(count)+'.png','rb')}
-                r = requests.post(url_img,files=files, headers=headers)
-                print(r)
-                time.sleep(2)
-                count=count+1
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
 
+    
+    if len(rects) == 0:
+        time.sleep(5)
+        print('nono')
+        cv2.imwrite('./Image/' + str(count) + '.png', frame)
+
+        data = {'msg': 'MOTOROFF'}
+        requests.post(ControlURL, data=json.dumps(data), headers=command_headers)
+
+        files = {'file': open('./Image/' + str(count) + '.png', 'rb')}
+        # upload img
+
+        r = requests.post(IMGURL, files=files, headers=IMGheaders)
+        time.sleep(2)
+        count = count + 1
                 
 
-	# loop over the face detections
-	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a NumPy
-		# array
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
+    
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
  
-		# extract the left and right eye coordinates, then use the
-		# coordinates to compute the eye aspect ratio for both eyes
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
  
-		# average the eye aspect ratio together for both eyes
-		ear = (leftEAR + rightEAR) / 2.0
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
 
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        # compute the convex hull for the left and right eye, then
+        # visualize each of the eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-		# check to see if the eye aspect ratio is below the blink
-		# threshold, and if so, increment the blink frame counter
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
 
-		
-		if ear < EYE_AR_THRESH:  
-                    sleep=1
-                    if sleep_start==0:
-                            sleep_start=time.time()
-                    
-		if sleep==1:
-                    if ear>EYE_AR_THRESH:
-                        sleep=0
-                    
+        
+        if ear < EYE_AR_THRESH:
+            sleep = 1
+
+            if sleep_start == 0:
+                sleep_start = time.time()
+       
+        if sleep == 1:
+            if ear > EYE_AR_THRESH:
+                sleep = 0
+        # sleep=0
+        else:
+            
+            if sleep_start != 0 and sleep_end == 0:
+                sleep_end = time.time()
+                sleep_time = caculate_sleep(sleep_start, sleep_end)
+
+                
+                if sleep_time > 5:
+                    print("wake up %0.2f" % ((sleep_time)))
+                
                 else:
-                    if sleep_start!=0 and sleep_end==0:
-                        sleep_end=time.time()
-                        sleep_time = caculate_sleep(sleep_start,sleep_end)
+                    sleep_start = 0
+                    sleep_end = 0
+            
+            else:
+                print("not sleep")
+                data = {'msg': 'MOTORON'}
+                motor_headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+                requests.post(ControlURL, data=json.dumps(data), headers=command_headers)
 
-                        if sleep_time>5:
-                                print("wake up %0.2f"%((sleep_time)))
-                        else:#sleep_time<5
-                                sleep_start=0
-                                sleep_end=0
-                        
-                    else:
-                        print("not sleep")
-                        
-
-
-               
-                       
-                 
-
+                
                 # draw the total number of blinks on the frame along with
                 # the computed eye aspect ratio for the frame
                 cv2.putText(frame, "Blinks: {}".format(EYE_AR_THRESH), (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) 
-	# show the frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-	
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
  
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+
