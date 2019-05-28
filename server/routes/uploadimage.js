631394ce3ae8f9adf277604109fa81be7eba9209
jinkkongee <43057166+jinkkongee@users.noreply.github.com>
2019-05-29 00:14:01 +0900

    파일이름 수정

--- server/routes/uploadimage.js
+++ server/routes/uploadimage.js
@@ -1,34 +1,51 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var uploads = multer({dest:'uploads/'})
+//var uploads = multer({dest:'uploads/'})
+
+var Imageupload = require('./../models/Imageupload.js');
 var mongoose = require('mongoose');
 var con = require('./../con');
 var mongo=con.mongo;
+mongoose.connect(mongo, {useNewUrlParser: true});
+
+
+
 var http = require('http').Server(express);
 var fs = require('fs');
 var exec = require('child_process').exec;
 var cmd='';
+var moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
+date = moment().format("MM.DD.HH.mm")
 
-//uploads라는 파일 미리 만들어야한다
-router.post('/',uploads.array('file'),function(req,res,next){
-  res.send();
-
-  //console.log('hi');
-});
-//저장 이름및 경로 설정
-let storage = multer.diskStorage({
+  let storage = multer.diskStorage({
   destination: function(req, file ,callback){
-    callback(null, "upload/")
+    callback(null, "uploads/")
   },
   filename: function(req, file, callback){
-    callback(null, file.originalname + " - " + Date.now())
+    callback(null,date+'-'+file.originalname)
   }
-})
+});
 
-let upload = multer({
+let uploads = multer({
   storage: storage
 })
 
 
+
+
+//uploads라는 파일 미리 만들어야한다
+router.post('/',uploads.single('file'),function(req,res,next){
+	//res.send('uploads:'+req.file.originalname);
+
+
+ res.send();
+  //console.log('hi');
+});
+//저장 이름및 경로 설정
+
+
+//module.exports = mongoose.model('Image',imageSchema);
 module.exports = router;
